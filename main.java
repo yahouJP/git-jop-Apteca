bbe4c4a5b3b2c52546508cbf744d66838f3f32c4
yahouJP <corambe@mail.ru>
2021-12-12 09:24:00 +0500

    otyicacelnye price i collivectvo

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main{
 	public static void main(String args[]){
 		Scanner s = new Scanner(System.in);
 		int colTovars;
-		String name;
+		StringBuilder name;
 		float price;
 		int collichestvo;
 
@@ -16,12 +16,23 @@ public class main{
 		for (int i = 1;i<=colTovars;i++) {
 			
 			System.out.print("Vvedite imya tovara №"+i+" : ");
-			name = s.nextLine();
+			name = new StringBuilder(s.nextLine());
 			System.out.print("Vvedite cenu tovara №"+i+" : ");
 			price = s.nextFloat();
+			if(price<0){
+				System.out.println("Cena tovara ne mojet byt otrecacelnoy.");
+				i--;
+				s.nextLine();
+				continue;
+			}
 			System.out.print("Vvedite collichestvo tovara №"+i+" : ");
 			collichestvo = s.nextInt();
 			s.nextLine();
+			if(collichestvo<0){
+				System.out.println("Collichestvo tovara ne mojet byt otrecacelnoy.");
+				i--;
+				continue;
+			}
 
 			towars[i-1][0]=name;
 			towars[i-1][1]=price;
