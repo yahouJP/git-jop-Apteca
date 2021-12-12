049b657581e5842f26d73ece6f75587a1fd9a874
yahouJP <corambe@mail.ru>
2021-12-12 12:53:47 +0500

    Udalenie tovara

--- main.java
+++ main.java
@@ -40,26 +40,36 @@ public class main{
 			towars[i-1][2]=collichestvo;
 		}
 
-		System.out.println("Dlya vyhoda iz programmy vvedite 0");
-		System.out.println("Dlya poisko towara vvedite 1");
+		for(;true;){
+			System.out.println("/-------------------------------------------/");
+			System.out.println("Dlya vyhoda iz programmy vvedite 0");
+			System.out.println("Dlya poiska towara vvedite 1");
+			System.out.println("Dlya udaleniya towara vvedite 2");
 		
-
-		System.out.print(">>> ");
-		vybor = s.nextByte();
-		s.nextLine();
-
-		if(vybor==1){
-		
-			System.out.println("Dlya poiska tovara po nazvaniyu vvedite 1");
-			System.out.println("Dlya poiska tovara po cene vvedite 2");
-			System.out.println("Dlya poiska tovara po collichestvu vvedite 3");
-
+			System.out.println("/-------------------------------------------/");
 			System.out.print(">>> ");
 			vybor = s.nextByte();
 			s.nextLine();
 
-			poisk(towars, vybor, s);
+			if(vybor==1){
+				System.out.println("/-------------------------------------------/");
+		
+				System.out.println("Dlya poiska tovara po nazvaniyu vvedite 1");
+				System.out.println("Dlya poiska tovara po cene vvedite 2");
+				System.out.println("Dlya poiska tovara po collichestvu vvedite 3");
+				System.out.println("/-------------------------------------------/");
 
+				System.out.print(">>> ");
+				vybor = s.nextByte();
+				s.nextLine();
+
+				poisk(towars, vybor, s);
+
+			}else if(vybor==2){
+				dellTowar(towars, s);
+			}else if(vybor==0){
+				break;
+			}
 		}
 
 	}
@@ -72,6 +82,7 @@ public class main{
 
 		int sovpodeniya = 0;
 		
+		System.out.println("/-------------------------------------------/");
 		if(vybor==1){
 
 			System.out.print("Vvedite nazvaniye iskomogo tovara: ");
@@ -139,6 +150,34 @@ public class main{
 
 		}
 		
+
+	}
+
+	public static void dellTowar(Object spisoc[][], Scanner s){
+
+		for(int i=0;i<spisoc.length;i++){
+			if(("sedrtfyghj8764".toString()).equals(spisoc[i][0].toString())){
+				continue;
+			}
+
+			System.out.println("Towar №"+(i+1)+": "+spisoc[i][0]);
+		}
+
+		System.out.println("/-------------------------------------------/");
+		System.out.print("Vvedite nazvanie towara dlya ego udaleniya: ");
+		namePoisk = new StringBuilder(s.nextLine());
+
+		for(int i=0;i<spisoc.length;i++){
+			
+			if((namePoisk.toString()).equals(spisoc[i][0].toString())){
+				System.out.println("Towar "+spisoc[i][0]+" udalen");
+				spisoc[i][0]="sedrtfyghj8764";
+				spisoc[i][1]=0;
+				spisoc[i][2]=0;
+			}
+		}
+
+
 
 	}
 
